7e74bc313f2293689ce9bfd9a2c391864d27e50c
egemen-dev <egemenwasd@gmail.com>
2023-07-15 23:06:19 +0300

    Make changes to listen requests after connecting to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,28 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
 // express app
 const app = express();
 
 // middleware
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+app.use(express.json());
+app.use("/api/workouts", workoutRoutes);
 
 // routes
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the backend!" });
 });
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("Server started on port, ", process.env.PORT);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port,", process.env.PORT);
+    });
+  })
+  .catch((err) => console.log(err));
