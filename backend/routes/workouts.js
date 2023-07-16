0632441f84eb028598b66d6e750d8980e5c4a8ed
egemen-dev <egemenwasd@gmail.com>
2023-07-16 12:06:55 +0300

    Refactor routes with new controller methods

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,37 +1,26 @@
+const {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  updateWorkout,
+  deleteWorkout,
+} = require("../controllers/workoutController");
 const express = require("express");
-const Workout = require("../models/workoutModel");
 const router = express.Router();
 
 // GET all workouts
-router.get("/", (req, res) => {
-  res.json({ message: "GET all workouts" });
-});
+router.get("/", getWorkouts);
 
 // GET one workout
-router.get("/:id", (req, res) => {
-  res.json({ message: "GET one workout" });
-});
+router.get("/:id", getWorkout);
 
 // POST one workout
-router.post("/", async (req, res) => {
-  const { title, reps, sets, weight } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, reps, sets, weight });
-    res.status(200).json(workout);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+router.post("/", createWorkout);
 
 // UPDATE one workout
-router.patch("/:id", (req, res) => {
-  res.json({ message: "UPDATE one workout" });
-});
+router.patch("/:id", updateWorkout);
 
 // DELETE one workout
-router.delete("/:id", (req, res) => {
-  res.json({ message: "DELETE one workout" });
-});
+router.delete("/:id", deleteWorkout);
 
 module.exports = router;
