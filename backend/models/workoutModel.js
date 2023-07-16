ff036d522f3f88f3ddd52d7f22d48ae367882080
egemen-dev <egemenwasd@gmail.com>
2023-07-17 00:01:41 +0300

    Change model schema

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -4,21 +4,47 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
   {
+    type: {
+      type: String,
+      enum: ["cardio", "lifting", "stretching", "cycling", "running", "walking"],
+      required: true,
+    },
     title: {
       type: String,
       required: true,
     },
     reps: {
       type: Number,
-      required: true,
+      default: 1,
+      required: function () {
+        return this.type === "lifting";
+      },
     },
     sets: {
       type: Number,
+      default: 1,
       required: true,
     },
     weight: {
       type: Number,
-      required: true,
+      required: function () {
+        return this.type === "lifting";
+      },
+    },
+    distance: {
+      type: Number,
+      required: function () {
+        return (
+          this.type === "cycling" || this.type === "running" || this.type === "walking"
+        );
+      },
+    },
+    duration: {
+      type: Number,
+    },
+    completed: {
+      type: Boolean,
+      default: true,
     },
   },
   { timestamps: true }
