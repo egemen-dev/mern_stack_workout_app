6670759550aeea0afb0c9e9a4aa6339007057c52
egemen-dev <egemenwasd@gmail.com>
2023-07-18 23:53:51 +0300

    Utilize custom hook to check if user is already logged in in useEffect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Pages and Components
@@ -6,12 +7,27 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
+import checkLogin from "./hooks/useCheckLogin";
+
+// TODO:
+// 1. Register a new user and save the token in localStorage - DONE
+// 2. Prevent logged in users from accessing the login and register pages - WIP
+// 3. Login a user and save the token in localStorage - WIP
+// 4. Logout a user and remove the token from localStorage - WIP
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  // Check if user is logged in
+  useEffect(() => {
+    checkLogin({ setUser });
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
+        <Navbar user={user} />
+        <p>{user && `${user.email} logged in`}</p>
         <div className="pages">
           <Routes>
             <Route path="/login" element={<Login />} />
