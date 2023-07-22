0a27e2704df2ceba13410b0bdbff7dac123c4fd9
egemen-dev <egemenwasd@gmail.com>
2023-07-23 00:02:45 +0300

    Update navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,28 +3,28 @@ import { Link } from "react-router-dom";
 
 const Navbar = ({ user }) => {
   return (
-    <header className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container">
-        <Link to="/" className="navbar-brand">
+    <div className="navbar bg-base-200 px-48">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
           <h1>Workout Tracker</h1>
         </Link>
-        <nav>
-          <div>
-            {!user && (
-              <>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-              </>
-            )}
-            {user && (
-              <Link to="/logout" onClick={() => localStorage.removeItem("user")}>
-                Logout
-              </Link>
-            )}
-          </div>
-        </nav>
       </div>
-    </header>
+      <div className="flex-none px-6">
+        <div className="flex gap-6">
+          {!user && (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
+          {user && (
+            <Link to="/logout" onClick={() => localStorage.removeItem("user")}>
+              Logout
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
