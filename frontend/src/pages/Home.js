56b00448735cd9c3b86f4f55b111bdbb667da3f1
egemen-dev <egemenwasd@gmail.com>
2023-07-26 11:58:56 +0300

    Remove fash after 5 sec

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -39,28 +39,46 @@ const Home = () => {
     fetchWorkouts();
   }, []);
 
+  const removeFlash = () => {
+    setTimeout(() => {
+      setFlash(null);
+    }, 5000);
+
+    return flash;
+  };
+
+  // Fix logout bug - DONE
+  // Better error style - DONE
+  // Remove console logs
+  // Fix login register from style bug
+  // Add user name above form
+
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-5 my-6">
-      {flash && <p className="error">{flash}</p>}
-      <div className="col-span-2 flex flex-col lg:justify-start items-center p-4 lg:pl-40 lg:pr-8">
-        <WorkoutForm
-          workouts={workouts}
-          setNewWorkouts={setNewWorkouts}
-          setFlashMessage={setFlash}
-        />
+    <>
+      <div className="w-full flex justify-center">
+        {flash && <div className="text-info pt-3">{removeFlash()}</div>}
       </div>
-      <div className="col-span-3 flex flex-col lg:justify-start items-center p-4 lg:pr-40 lg:pl-8">
-        {workouts &&
-          workouts.map((workout) => (
-            <WorkoutCard
-              workout={workout}
-              key={workout._id}
-              workouts={workouts}
-              setNewWorkouts={setNewWorkouts}
-            />
-          ))}
+      <div className="grid grid-cols-1 lg:grid-cols-5 my-6">
+        <div className="col-span-2 flex flex-col lg:justify-start items-center p-4 lg:pl-40 lg:pr-8">
+          <WorkoutForm
+            workouts={workouts}
+            setNewWorkouts={setNewWorkouts}
+            setFlashMessage={setFlash}
+          />
+        </div>
+        <div className="col-span-3 flex flex-col lg:justify-start items-center p-4 lg:pr-40 lg:pl-8">
+          {workouts &&
+            workouts.map((workout) => (
+              <WorkoutCard
+                workout={workout}
+                key={workout._id}
+                workouts={workouts}
+                setNewWorkouts={setNewWorkouts}
+              />
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
